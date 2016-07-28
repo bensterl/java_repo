010edec88b910dc67cdcf1614707929d43a2bb48
Ben Sterling <bsterling@SterlingTech.internal>
2016-07-28 19:11:23 -0400

    Added the Apples class

---
+++ Apples.java
@@ -0,0 +1,15 @@
+class Apples
+{
+	public static void main(String args[])
+	{
+		Bananas b = new Bananas();
+	}
+}
+
+class Bananas
+{
+	public Bananas()
+	{
+		System.out.println("I'm bananas");
+	}
+}
